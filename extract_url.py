e97eaf97ea6ee77e97311bb6a9b848947af31c86
andy <635885852@qq.com>
2019-10-08 16:41:13 +0800

    upgrade

--- extract_url.py
+++ extract_url.py
@@ -1,13 +1,54 @@
 import json
-
+import requests
 
 json_path = './data.json'
+
+UA = {
+    "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
+}
+
+url = 'https://live.kuaishou.com/graphql'
 
 with open(json_path,'r',encoding='utf-8') as f:
     data = json.loads(f.read())
 
-lists = data['data']['getProfileFeeds']['list']
-tmp_data = [x['playUrl'] for x in lists]
+# 快手改了，所以先拿photoId，然后请求真实地址
+lists = data['data']['publicFeeds']['list']
+author = data['data']['publicFeeds']['list'][0]['user']['id']
+
+photoIds = []
+for x in lists:
+    photoIds.append(x['photoId'])
+
+query = '''query SharePageQuery($principalId: String, $photoId: String) {
+  feedById(principalId: $principalId, photoId: $photoId) {
+    currentWork {
+      playUrl
+      __typename
+    }
+    __typename
+  }
+}
+'''
+
+# 如果不能用，浏览器里面找到cookie，自己替换即可
+cookies = {
+  'did':'web_499b1d56248e45f79ee28125cd0508b8',
+  'client_key':'65890b29'
+}
+
+playUrls = []
+for x in photoIds:
+    print(x)
+    payload = {"operationName":"SharePageQuery",
+                "variables":{"photoId":x,"principalId":author},
+                "query":query}
+
+    res = requests.post(url=url,json=payload,headers=UA,cookies=cookies)
+    data = json.loads(res.text)
+    playUrl = data['data']['feedById']['currentWork']['playUrl']
+    playUrls.append(playUrl)
 
 with open('video.json','w',encoding='utf-8') as f:
-    f.write(json.dumps(tmp_data, indent=4, separators=(',', ':')))
+    f.write(json.dumps(playUrls, indent=4, separators=(',', ':')))
--- get_data.py
+++ get_data.py
@@ -8,97 +8,92 @@ UA = {
 
 url = 'https://live.kuaishou.com/graphql'
 
-query = """query ProfileFeeds($principalId: String, $privacy: String, $pcursor: String, $count: Int) {
-  getProfileFeeds(principalId: $principalId, privacy: $privacy, pcursor: $pcursor, count: $count) {
+query = '''
+query publicFeedsQuery($principalId: String, $pcursor: String, $count: Int) {
+  publicFeeds(principalId: $principalId, pcursor: $pcursor, count: $count) {
     pcursor
     live {
-      ...LiveStreamInfo
+      user {
+        id
+        kwaiId
+        eid
+        profile
+        name
+        living
+        __typename
+      }
+      watchingCount
+      src
+      title
+      gameId
+      gameName
+      categoryId
+      liveStreamId
+      playUrls {
+        quality
+        url
+        __typename
+      }
+      followed
+      type
+      living
+      redPack
+      liveGuess
+      anchorPointed
+      latestViewed
+      expTag
       __typename
     }
     list {
-      ...WorkInfo
+      photoId
+      caption
+      thumbnailUrl
+      poster
+      viewCount
+      likeCount
+      commentCount
+      timestamp
+      workType
+      type
+      useVideoPlayer
+      imgUrls
+      imgSizes
+      magicFace
+      musicName
+      location
+      liked
+      onlyFollowerCanComment
+      relativeHeight
+      width
+      height
+      user {
+        id
+        eid
+        name
+        profile
+        __typename
+      }
+      expTag
       __typename
     }
     __typename
   }
 }
-
-fragment LiveStreamInfo on LiveStream {
-  user {
-    id
-    kwaiId
-    eid
-    profile
-    name
-    living
-    __typename
-  }
-  watchingCount
-  src
-  title
-  gameId
-  gameName
-  categoryId
-  liveStreamId
-  playUrls {
-    quality
-    url
-    __typename
-  }
-  followed
-  type
-  living
-  redPack
-  liveGuess
-  anchorPointed
-  latestViewed
-  expTag
-  __typename
-}
-
-fragment WorkInfo on VideoFeed {
-  photoId
-  caption
-  thumbnailUrl
-  poster
-  viewCount
-  likeCount
-  commentCount
-  timestamp
-  workType
-  type
-  playUrl
-  useVideoPlayer
-  imgUrls
-  imgSizes
-  magicFace
-  musicName
-  location
-  liked
-  onlyFollowerCanComment
-  relativeHeight
-  width
-  height
-  user {
-    id
-    name
-    profile
-    __typename
-  }
-  expTag
-  __typename
-}
-"""
-
+'''
+# https://live.kuaishou.com/profile/3xdqruzfay95yys
 author = input('输入主播的ID：')
 if not author:
     author = '3xdqruzfay95yys'
-# "pcursor":"1.544090000891E12"
-payload = {"operationName":"ProfileFeeds",
-            "variables":{"principalId":author,"privacy":"public","count":100},
+payload = {"operationName":"publicFeedsQuery",
+            "variables":{"principalId":author,"pcursor":"","count":10000},
             "query":query}
 
-res = requests.post(url=url,json=payload,headers=UA)
+# 如果不能用，浏览器里面找到cookie，自己替换即可
+cookies = {
+  'did':'web_499b1d56248e45f79ee28125cd0508b8',
+  'client_key':'65890b29'
+}
+res = requests.post(url=url,json=payload,headers=UA,cookies=cookies)
 data = json.loads(res.text)
 
 with open('./data.json','w',encoding='utf-8') as f:
